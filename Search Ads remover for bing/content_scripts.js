0ba1fc5e6a3a2d6c49ae81f7a703f3755b743227
kiyoken1594 <kiyokenchoukai1594@outlook.jp>
2024-07-04 23:25:17 +0900

    It enabled to remove Ads on image-search.

--- Search Ads remover for bing/content_scripts.js
+++ Search Ads remover for bing/content_scripts.js
@@ -1,13 +1,26 @@
-window.onload = function () {
-        const adlist_A = document.querySelectorAll('#b_results>li.b_ad');
-        const adArray_A = Array.from(adlist_A);
+function removeADs() {
+        let adlist_A = document.querySelectorAll('#b_results>li.b_ad');
+        let adArray_A = Array.from(adlist_A);
         for (let i = 0; i < adArray_A.length; i++) {
                 adArray_A[i].remove();
         };
-        const regex = /^(?!title|subtitle)[a-z]{2,10}$/;
-        const adlist_B = document.querySelectorAll('p');
-        const adArray_B = Array.from(adlist_B).filter(div => regex.test(div.className));
+        let regex = /^(?!title|subtitle)[a-z]{2,10}$/;
+        let adlist_B = document.querySelectorAll('p');
+        let adArray_B = Array.from(adlist_B).filter(div => regex.test(div.className));
         for (let i = 0; i < adArray_B.length; i++) {
                 adArray_B[i].parentElement.parentElement.remove();
         };
+};
+function removeimageADs(){
+        let ad_on_image =  document.querySelectorAll('div.ta_c');
+        let adArray_image = Array.from(ad_on_image);
+        for (let i = 0; i < adArray_image.length; i++) {
+                adArray_image[i].remove();
+        };
+};
+window.onload = function(){
+        removeADs;
+        setInterval(removeADs, 500);
+        removeimageADs;
+        setInterval(removeimageADs, 250);
 };
